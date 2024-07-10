05472e1dbef7cb3b93645bd60d1bedbee2764d83
PizzaLovingNerd <cameron@stillhq.io>
2024-07-10 14:09:21 -0700

    Implementing spin rows (for number inputs)

--- GSettingGroup.py
+++ GSettingGroup.py
@@ -1,3 +1,4 @@
+import enum
 import os
 
 import constants
@@ -7,6 +8,14 @@ import gi
 gi.require_version("Gtk", "4.0")
 gi.require_version("Adw", "1")
 from gi.repository import Adw, Gio, GObject, Gtk
+
+
+class SpinType(enum.Enum):
+    INT = 0
+    UINT = 1
+    DOUBLE = 2
+
+
 
 
 class GSetting(GObject.GObject):
@@ -64,21 +73,69 @@ class GSetting(GObject.GObject):
 class GSettingsGroup(Adw.PreferencesGroup):
     __gtype_name__ = "GSettingsGroup"
 
-    def add_boolean(self, gsetting: GSetting):
-        switch_row = Adw.SwitchRow(title=gsetting.title, subtitle=gsetting.subtitle)
+    def add_switch(self, gsetting: GSetting):
+        row = Adw.SwitchRow(title=gsetting.title, subtitle=gsetting.subtitle)
         if gsetting.icon_name:
-            switch_row.set_icon_name(gsetting.icon_name)
-        switch_row.set_active(gsetting.settings.get_boolean(gsetting.key))
-        switch_row.connect("notify::active", boolean_row_changed, gsetting)
-        gsetting.settings.connect("changed", boolean_setting_changed, switch_row, gsetting)
-        self.add(switch_row)
+            row.set_icon_name(gsetting.icon_name)
+        row.set_active(gsetting.settings.get_boolean(gsetting.key))
+        row.connect("notify::active", switch_row_changed, gsetting)
+        gsetting.settings.connect("changed", switch_setting_changed, row, gsetting)
+        self.add(row)
+
+    def add_spin(self, gsetting: GSetting, spin_type, percent, adjustment):
+        row = Adw.SpinRow(title=gsetting.title, subtitle=gsetting.subtitle)
+        if gsetting.icon_name:
+            row.set_icon_name(gsetting.icon_name)
+
+        row.set_adjustment(adjustment)
+        if not percent:
+            row.set_digits(3)
+
+        spin_setting_changed(
+            gsetting.settings, gsetting.key, row, gsetting, spin_type, percent
+        )
+
+        row.connect("notify::value", spin_row_changed, gsetting, spin_type, percent)
+        gsetting.settings.connect("changed", spin_setting_changed, row, gsetting, spin_type, percent)
+        self.add(row)
 
 
-def boolean_row_changed(switch_row, _active, gsetting):
+def switch_row_changed(switch_row, _active, gsetting):
     gsetting.settings.set_boolean(gsetting.key, switch_row.get_active())
 
 
-def boolean_setting_changed(settings, key, switch_row, gsetting):
+def switch_setting_changed(settings, key, switch_row, gsetting):
     if key == gsetting.key:
         if switch_row.get_active() != settings.get_boolean(key):
             switch_row.set_active(settings.get_boolean(key))
+
+
+def spin_row_changed(spin_row, _value, gsetting, spin_type, percent):
+    value = spin_row.get_value()
+    if percent:
+        value /= 100
+
+    match spin_type:
+        case SpinType.INT:
+            gsetting.settings.set_int(gsetting.key, value)
+        case SpinType.UINT:
+            gsetting.settings.set_uint(gsetting.key, value)
+        case SpinType.DOUBLE:
+            gsetting.settings.set_double(gsetting.key, value)
+
+
+def spin_setting_changed(settings, key, spin_row, gsetting, spin_type, percent):
+    if key == gsetting.key:
+        match spin_type:
+            case SpinType.INT:
+                new_value = settings.get_int(key)
+            case SpinType.UINT:
+                new_value = settings.get_uint(key)
+            case SpinType.DOUBLE:
+                new_value = settings.get_double(key)
+
+        if spin_row.get_value() != float(new_value):
+            if percent:
+                new_value *= 100
+            spin_row.set_value(new_value)
+
--- __main__.py
+++ __main__.py
@@ -1,10 +1,15 @@
+import gi
+gi.require_version("Gtk", "4.0")
+
+from gi.repository import Gtk
+
 import stillControl
 import GSettingGroup
 
 if __name__ == "__main__":
     app = stillControl.StillControl()
 
-    app.builder.get_object("theming_group").add_boolean(
+    app.builder.get_object("theming_group").add_switch(
         GSettingGroup.GSetting(
             title="Show Battery Percentage",
             subtitle="This shows the battery percentage in the top bar. This is only useful if you have a laptop.",
@@ -12,5 +17,16 @@ if __name__ == "__main__":
             schema="org.gnome.desktop.interface",
             key="show-battery-percentage"
         )
+    )
+    app.builder.get_object("theming_group").add_spin(
+        GSettingGroup.GSetting(
+            title="Font Scaling Factor",
+            subtitle="The scaling factor for font sizes.",
+            icon_name="weather-clear-night-symbolic",
+            schema="org.gnome.desktop.interface",
+            key="text-scaling-factor"
+        ),
+        GSettingGroup.SpinType.DOUBLE, True,
+        Gtk.Adjustment(lower=50, upper=300, step_increment=25)
     )
     app.run()
