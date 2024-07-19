2471c194e2829bee1556e55fae4d4a4a55e70d5c
PizzaLovingNerd <cameron@stillhq.io>
2024-07-18 17:22:31 -0700

    Added gsetting

--- stillControl.py
+++ stillControl.py
@@ -8,6 +8,7 @@ from gi.repository import Gtk, Adw
 
 from __init__ import GSetting  # FIXME: Change this to absolute import
 
+
 class StillControl(Adw.Application):
     def __init__(self):
         super().__init__(application_id="io.stillhq.stillControl")
@@ -16,10 +17,10 @@ class StillControl(Adw.Application):
         self.builder.add_from_file(os.path.join(constants.UI_DIR, "stillControl.ui"))
 
         self.main_window = self.builder.get_object("main_window")
-        self.setup_extension_page()
+        self.setup_manage_extension_page()
         # self.connect("activate", self.do_activate)
 
-    def setup_extension_page(self):
+    def setup_manage_extension_page(self):
         self.extension_group = self.builder.get_object("extension_group")
         third_party_extensions = self.extension_group.add_switch_inverse(
             GSetting(
@@ -28,9 +29,21 @@ class StillControl(Adw.Application):
                 schema="org.gnome.shell", key="disable-user-extensions"
             )
         )
-        warning_image = Gtk.Image.new_from_icon_name("dialog-warning-symbolic")
-        warning_image.add_css_class("warning")
-        third_party_extensions.add_prefix(warning_image)
+        system_extensions = self.extension_group.add_switch(
+            GSetting(
+                title="Show System Extensions",
+                subtitle="These extensions are used by stillOS's UI. Disabling them may cause problems.",
+                schema="io.stillhq.control", key="show-system-extensions"
+            )
+        )
+        third_party_warning_image = Gtk.Image.new_from_icon_name("dialog-warning-symbolic")
+        third_party_warning_image.add_css_class("warning")
+        system_extensions_warning_image = Gtk.Image.new_from_icon_name("dialog-warning-symbolic")
+        system_extensions_warning_image.add_css_class("warning")
+        system_extensions.add_prefix(system_extensions_warning_image)
+        third_party_extensions.add_prefix(third_party_warning_image)
+
+
 
     def do_activate(self):
         self.main_window.set_application(self)
