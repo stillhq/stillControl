f81e5f514d23262c420ca4f77314ff6f6120d59e
PizzaLovingNerd <cameron@stillhq.io>
2024-08-05 21:00:02 -0600

    Fixed the layouts for more consistency

--- data/utils/create_layout.py
+++ data/utils/create_layout.py
@@ -15,19 +15,8 @@ def serialize_setting(setting, key):
 shell_settings = Gio.Settings.new("org.gnome.shell")
 
 panel_settings = Gio.Settings.new("org.gnome.shell.extensions.dash-to-panel")
-panel_keys = [
-    "intellihide",
-    "appicon-margin",
-    "appicon-padding",
-    "focus-highlight",
-    "dot-position",
-    "dot-style-unfocused",
-    "dot-style-focused",
-    "show-favorites",
-    "show-running-apps",
-    "group-apps",
-    "group-apps-use-launchers",
-    "stockgs-keep-top-panel",
+panel_exclude = [
+    "available-monitors"
 ]
 panel_monitor_keys = [
     "panel-anchors", "panel-element-positions",
@@ -35,124 +24,14 @@ panel_monitor_keys = [
 ]
 
 dock_settings = Gio.Settings.new("org.gnome.shell.extensions.dash-to-dock")
-dock_keys = [
-    "intellihide",
-    "extend-height",
-    "dock-position",
-    "dash-max-icon-size",
-    "height-fraction",
-    "custom-theme-shrink",
-    "show-favorites",
-    "show-running",
-    "isolate-monitors",
-    "show-show-apps-button",
-    "show-trash",
-    "show-mounts",
-    "show-windows-preview",
-    "show-apps-at-top",
-    "show-apps-always-in-the-edge",
-
+dock_exclude = [
+    "preferred-monitor",
+    "preferred-monitor-by-connector"
 ]
 arc_settings = Gio.Settings.new("org.gnome.shell.extensions.arcmenu")
-arc_keys = arc_menu_settings = [
-    "activate-on-hover",
-    "all-apps-button-action",
-    "alphabetize-all-programs",
-    "apps-show-extra-details",
-    "arcmenu-extra-categories-links",
-    "arcmenu-extra-categories-links-location",
-    "arcmenu-hotkey",
-    "avatar-style",
-    "az-layout-extra-shortcuts",
-    "brisk-layout-extra-shortcuts",
-    "button-item-icon-size",
-    "button-padding",
-    "category-icon-type",
-    "custom-grid-icon-size",
-    "custom-menu-button-icon-size",
-    "custom-menu-button-text",
-    "dash-to-panel-standalone",
-    "default-menu-view",
-    "default-menu-view-redmond",
-    "default-menu-view-tognee",
-    "disable-recently-installed-apps",
-    "disable-scrollview-fade-effect",
-    "disable-tooltips",
-    "disable-user-avatar",
-    "distro-icon",
-    "eleven-disable-frequent-apps",
-    "eleven-layout-extra-shortcuts",
-    "enable-activities-shortcut",
-    "enable-clock-widget-raven",
-    "enable-clock-widget-unity",
-    "enable-horizontal-flip",
-    "enable-unity-homescreen",
-    "enable-weather-widget-raven",
-    "enable-weather-widget-unity",
-    "extra-categories",
-    "force-menu-location",
-    "gnome-dash-show-applications",
-    "hide-overview-on-startup",
-    "highlight-search-result-terms",
-    "hotkey-open-primary-monitor",
-    "insider-layout-extra-shortcuts",
-    "left-panel-width",
-    "max-search-results",
-    "menu-arrow-rise",
-    "menu-button-appearance",
-    "menu-button-icon",
-    "menu-button-left-click-action",
-    "menu-button-middle-click-action",
-    "menu-button-position-offset",
-    "menu-button-right-click-action",
-    "menu-font-size",
-    "menu-height",
-    "menu-layout",
-    "menu-position-alignment",
-    "menu-width-adjustment",
-    "mint-layout-extra-shortcuts",
-    "misc-item-icon-size",
-    "multi-lined-labels",
-    "multi-monitor",
-    "override-menu-theme",
-    "plasma-enable-hover",
-    "pop-default-view",
-    "pop-folders-data",
-    "position-in-panel",
-    "power-display-style",
-    "quicklinks-item-icon-size",
-    "raven-position",
-    "raven-search-display-style",
-    "right-panel-width",
-    "runner-font-size",
-    "runner-hotkey",
-    "runner-hotkey-open-primary-monitor",
-    "runner-menu-height",
-    "runner-menu-width",
-    "runner-position",
-    "runner-search-display-style",
-    "runner-show-frequent-apps",
-    "search-entry-border-radius",
-    "search-provider-open-windows",
-    "search-provider-recent-files",
-    "searchbar-default-bottom-location",
-    "searchbar-default-top-location",
-    "settings-height",
-    "settings-width",
-    "shortcut-icon-type",
-    "show-activities-button",
-    "show-bookmarks",
-    "show-category-sub-menus",
-    "show-external-devices",
-    "show-hidden-recent-files",
-    "show-search-result-details",
-    "sleek-layout-extra-shortcuts",
-    "sleek-layout-panel-width",
-    "unity-layout-extra-shortcuts",
-    "vert-separator",
-    "windows-disable-frequent-apps",
-    "windows-disable-pinned-apps",
-    "windows-layout-extra-shortcuts"
+arc_exclude = [
+    "application-shortcuts",
+    "pinned-apps",
 ]
 panel_layouts = [
     'Default',
@@ -194,34 +73,30 @@ enabled_extensions = shell_settings.get_strv("enabled-extensions")
 
 if "dash-to-panel@jderose9.github.com" in enabled_extensions:
     layout_dict["panel"] = {}
-    for key in panel_monitor_keys:
-        try:
-            layout_dict["panel"][key] = get_panel_monitor_setting(key)
-        except:
-            print("Skipping panel key: " + key)
-            layout_dict["panel"][key] = {}
-            print("value:\n" + panel_settings.get_string(key) + "\n")
-    for key in panel_keys:
-        layout_dict["panel"][key] = serialize_setting(panel_settings, key)
+    for key in panel_settings.props.settings_schema.list_keys():
+        if key not in panel_exclude and not panel_settings.get_value(key).equal(panel_settings.get_default_value(key)):
+            if key in panel_monitor_keys:
+                try:
+                    layout_dict["panel"][key] = get_panel_monitor_setting(key)
+                except:
+                    print("Skipping panel key: " + key)
+                    layout_dict["panel"][key] = {}
+                    print("value:\n" + panel_settings.get_string(key) + "\n")
+            else:
+                layout_dict["panel"][key] = serialize_setting(panel_settings, key)
 
 if "dash-to-dock@micxgx.gmail.com" in enabled_extensions:
     layout_dict["dock"] = {}
-    for key in dock_keys:
-        layout_dict["dock"][key] = serialize_setting(dock_settings, key)
+    for key in dock_settings.props.settings_schema.list_keys():
+        if key not in dock_exclude and not dock_settings.get_value(key).equal(dock_settings.get_default_value(key)):
+            layout_dict["dock"][key] = serialize_setting(dock_settings, key)
 
 if "arcmenu@arcmenu.com" in enabled_extensions:
-    current_layout = arc_settings.get_string("menu-layout")
     layout_dict["arc"] = {}
-    for key in arc_keys:
-        # Make sure layout specifc settings are not included
-        commit_key = True
-        for layout in panel_layouts:
-            if key.startswith(layout.lower()) and layout != current_layout:
-                commit_key = False
-                break
-
-        if commit_key:
+    for key in arc_settings.props.settings_schema.list_keys():
+        if key not in arc_exclude and not arc_settings.get_value(key).equal(arc_settings.get_default_value(key)):
             layout_dict["arc"][key] = serialize_setting(arc_settings, key)
+
 
 filename = f"{layout_name.replace(" ", "_").lower()}.json"
 with open(filename, "w") as f:
